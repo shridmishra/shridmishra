f7139a4c1c2f80eec7eec368df7e46787d696667
shridmishra <shridmishra00@gmail.com>
2024-09-14 17:15:31 +0530

    added prpjects

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+export const Header = () => {
+
+    return <div className="bg-black font-sans h-0 ">
+   <div className="flex justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-0">
+  <div className="text-center text-white text-4xl md:text-6xl font-bold px-6 py-4 bg-opacity-80 bg-black rounded-lg shadow-lg">
+    <div className="font-extralight font-poppins">Hi, I'm </div><strong className="text-yellow-400">Shrid Mishra</strong>
+  </div>
+</div>
+
+             
+    </div>
+}
---
+++ src/components/Lamp.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import LampDemo from "./ui/lamp";
+
+
+export const Header = () => {
+
+    return <div className="bg-black font-sans h-0 ">
+  
+        <LampDemo/>
+             
+    </div>
+}
