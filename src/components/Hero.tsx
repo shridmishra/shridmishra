9979292fcb7dcb50c7d6a35a92e4062d2c9ade14
shridmishra <shridmishra00@gmail.com>
2025-08-11 18:25:23 +0530

    hero done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,7 +57,7 @@ export const Hero = () => {
 
           <div className="flex items-center gap-3">
             <MapPin className="w-5 h-5 text-muted-foreground flex-shrink-0" />
-            <Text text="Mumbai, India" />
+            <Text text="Mumbai, IN" />
 
 
           </div>
